b376ebaab89be15a358e320e979a36dd741d6052
Kate Vong <katiejnete@gmail.com>
2024-06-01 21:38:16 -0700

    complete testing invoice routes

--- routes/invoices.js
+++ routes/invoices.js
@@ -49,7 +49,7 @@ router.post("/", validateInvoice, findCompany, async (req, res, next) => {
   }
 });
 
-router.put("/:id", validateInvoice, findInvoice, async (req, res, next) => {
+router.put("/:id",findInvoice, validateInvoice, async (req, res, next) => {
   try {
     const { amt, paid } = req.body;
     let paid_date;
